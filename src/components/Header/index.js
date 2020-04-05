9253473ea2e3b14f278e56ac38f234ee073ef291
Maria Francis-Moullier <moullier@gmail.com>
2020-04-04 23:04:56 -0700

    Attempting to fix relative links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+    const location = useLocation();
     return (
         <div className="row" id="headerRow">
             <div className="col-md-6 col-12 justify-content-center d-flex justify-content-md-start">
@@ -10,13 +12,22 @@ function Header() {
                 <div className="nav justify-content-center justify-content-md-end" id="navlist">
                     <ul className="nav">
                         <li className="nav-item">
-                          <a className="nav-link active" href="/about">About</a>
+                            <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                                About
+                            </Link>
+                          {/* <a className="nav-link active" href="/about">About</a> */}
                         </li>
                         <li className="nav-item">
-                          <a className="nav-link" href="/projects">Projects</a>
+                            <Link to="projects" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                                Projects
+                            </Link>
+                          {/* <a className="nav-link" href="/projects">Projects</a> */}
                         </li>
                         <li className="nav-item">
-                          <a className="nav-link" href="/contact">Contact Me</a>
+                            <Link to="contact" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                                Contact Me
+                            </Link>
+                          {/* <a className="nav-link" href="/contact">Contact Me</a> */}
                         </li>
                       </ul>
                 </div>
