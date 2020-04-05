fada998cbb23ee7be91bc1f5c3513c3bd8230aec
Maria Francis-Moullier <moullier@gmail.com>
2020-04-04 22:45:30 -0700

    Updates

---
+++ precache-manifest.4d9ad7a8c733a068bd37808be883d771.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bd22869e69cfbd12ea1f7f8f7f833f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ac5ab4acf066868f1531",
+    "url": "/react-portfolio/static/css/main.d75f14db.chunk.css"
+  },
+  {
+    "revision": "c49b08ffa42631677ca9",
+    "url": "/react-portfolio/static/js/2.eba65d89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.eba65d89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5ab4acf066868f1531",
+    "url": "/react-portfolio/static/js/main.bfc95f3b.chunk.js"
+  },
+  {
+    "revision": "eff633beff9fe05cfab3",
+    "url": "/react-portfolio/static/js/runtime-main.6b1a3ce1.js"
+  },
+  {
+    "revision": "4103e9c5b075a162e822b40ac73b707b",
+    "url": "/react-portfolio/static/media/Maria Francis-Moullier - Resume - Web Developer.4103e9c5.pdf"
+  },
+  {
+    "revision": "1242bbae793fc91a68f3e204bda1a73e",
+    "url": "/react-portfolio/static/media/dayplanner_ss.1242bbae.png"
+  },
+  {
+    "revision": "dc95899087dd4307ede81e6e564f4f03",
+    "url": "/react-portfolio/static/media/maria.dc958990.jpg"
+  },
+  {
+    "revision": "0c2216477651a9f11641020b79622232",
+    "url": "/react-portfolio/static/media/pickem_ss.0c221647.png"
+  },
+  {
+    "revision": "60d83fecce87ecae3da2ba86f86e3258",
+    "url": "/react-portfolio/static/media/recipe_ss.60d83fec.png"
+  },
+  {
+    "revision": "93c53f81efb5ade6e0a8279e150a2e5d",
+    "url": "/react-portfolio/static/media/weatherss.93c53f81.png"
+  }
+]);
